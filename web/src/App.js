170d4374ac48dc9061c4cb8f9d5a0269ee5ff03e
lloma93 <palloma.adolfi@gmail.com>
2020-02-26 00:20:31 -0300

    layout css

--- web/src/App.js
+++ web/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './global.css'
 import './App.css'
 import './Sidebar.css'
+import './Main.css'
 
 function App() {
   return (
@@ -32,7 +33,52 @@ function App() {
         </form>
       </aside>
       <main>
-
+        <ul>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars2.githubusercontent.com/u/35180706?v=4" alt="Palloma" />
+              <div className="user-info">
+                <strong> Palloma Adolfi</strong>
+                <span> ReactJS, React, Vue</span>
+              </div>
+            </header>
+            <p>Dev front-end</p>
+            <a href="https://github.com/lloma93">Acessar perfil no Github</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars2.githubusercontent.com/u/35180706?v=4" alt="Palloma" />
+              <div className="user-info">
+                <strong> Palloma Adolfi</strong>
+                <span> ReactJS, React, Vue</span>
+              </div>
+            </header>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <a href="https://github.com/lloma93">Acessar perfil no Github</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars2.githubusercontent.com/u/35180706?v=4" alt="Palloma" />
+              <div className="user-info">
+                <strong> Palloma Adolfi</strong>
+                <span> ReactJS, React, Vue</span>
+              </div>
+            </header>
+            <p>Dev front-end</p>
+            <a href="https://github.com/lloma93">Acessar perfil no Github</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars2.githubusercontent.com/u/35180706?v=4" alt="Palloma" />
+              <div className="user-info">
+                <strong> Palloma Adolfi</strong>
+                <span> ReactJS, React, Vue</span>
+              </div>
+            </header>
+            <p>Dev front-end</p>
+            <a href="https://github.com/lloma93">Acessar perfil no Github</a>
+          </li>
+        </ul>
       </main>
     </div>
   )
